aa8ea6b0b0cb0e2f96e65db9035737f4d9b5158b
Bhuvipalanimalai <132568187+Bhuvipalanimalai@users.noreply.github.com>
2023-07-12 10:47:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_btn = document.getElementsById("on");
+var off_btn=document.getElementById("off");
+var image = document.getElementById("img");
+
+
+function bulb_on(){
+    img.src="./on.jpg";
+}
+function bulb_off(){
+    img.src="./off.jpg";
+}
